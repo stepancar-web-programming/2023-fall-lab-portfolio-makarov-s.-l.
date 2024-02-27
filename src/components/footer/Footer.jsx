a76f1ce92a1e73295df85e91339a439438db311f
semonmakarov <semon.makarov.2003@mail.ru>
2024-02-27 15:19:13 +0300

    Footer
    Создание футера

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./footer.css"
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="footer">
+            <nav className="container footer-container">
+                <p className="created-by">© created by Makarov Semen</p>
+                <ul className="footer-list">
+                    <li className="footer-item">
+                        <a href="/" className="footer-link">
+                            <i class="fi fi-brands-vk footer-icon"></i>
+                        </a>
+                    </li>
+
+                    <li className="footer-item">
+                        <a href="/" className="footer-link">
+                            <i class="fi fi-brands-telegram footer-icon"></i>
+                        </a>
+                    </li>
+
+                    <li className="footer-item">
+                        <a href="/" className="footer-link">
+                            <i class="fi fi-brands-github footer-icon"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
 };
 
 export default Footer;
